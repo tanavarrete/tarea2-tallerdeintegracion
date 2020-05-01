3911d321df7f5455a2b043994770c8c038669afd
TOMAS NAVARRETE <tanavarrete@uc.cl>
2020-05-01 17:31:01 -0400

    final fix

--- src/modules/ingrediente-module.js
+++ src/modules/ingrediente-module.js
@@ -2,9 +2,11 @@ let mongoose = require('mongoose')
 
 const { Schema } = mongoose;
 
+const autoIncrementModelID = require("./counter-module");
+
 const IngredienteSchema = new Schema(
   {
-    id: { type: Number, required: true, unique:true },
+    id: { type: Number, unique:true },
     nombre: { type: String, required: true },
     descripcion: { type: String, required: true },
     path: { type: String , default: 'https://rocky-eyrie-23489.herokuapp.com/ingrediente/'},
@@ -12,6 +14,15 @@ const IngredienteSchema = new Schema(
   },
   { versionKey: false }
 );
+
+IngredienteSchema.pre("save", function (next) {
+  if (!this.isNew) {
+    next();
+    return;
+  }
+
+  autoIncrementModelID("activities", this, next);
+});
 
 const model = mongoose.model('Ingrediente', IngredienteSchema);
 module.exports = model;
