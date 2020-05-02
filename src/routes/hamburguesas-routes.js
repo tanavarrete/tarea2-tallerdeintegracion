8507da851c9c2e956f89765e6a51c754c5e6209f
TOMAS NAVARRETE <tanavarrete@uc.cl>
2020-05-02 12:57:03 -0400

    hambutguesareturn

--- src/routes/hamburguesas-routes.js
+++ src/routes/hamburguesas-routes.js
@@ -132,8 +132,8 @@ router.delete(
         { $pullAll: { ingredientes: [ingrediente._id] } },
         { new: true } 
       );
-      const update2 = await Hamburguesa.findOneAndUpdate(
-        { _id: hamburguesa._id },
+      const update2 = await Ingrediente.findOneAndUpdate(
+        { _id: ingrediente._id },
         { $pullAll: { hamburguesas: [hamburguesa._id] } },
         { new: true } 
       );
